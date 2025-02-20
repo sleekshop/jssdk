e81aff8eed4ab48df594a5136000c1b9d6117957
Torsten Högel <torsten.hoegel@icloud.com>
2025-02-20 18:35:15 +0100

    fixed small issue in orders

--- src/routes/orders.ts
+++ src/routes/orders.ts
@@ -13,7 +13,7 @@ export default class Orders {
     constructor(parentObj: Sleekshop) {
         this.licence_username = parentObj.licence_username;
         this.licence_password = parentObj.licence_password;
-        this.licence_password = parentObj.licence_secret_key;
+        this.licence_secret_key = parentObj.licence_secret_key;
         this.instance = parentObj.instance;
         this.default_language = parentObj.default_language || "de_DE";
     }
